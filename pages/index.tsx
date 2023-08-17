4a437c9c18b4c4f29eca2ec906fc6ee6416d1a42
naarkz <naarkz@michie.dev>
2023-08-17 16:15:28 +0800

    feat: Add pagination to platform list

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,11 @@ import MainLayout from "../components/layouts/MainLayout";
 import { PlatformSection } from "../types/Zesty";
 import parser from "html-react-parser";
 
+const PAGE_SIZE = 10;
+
 export default function HomePage() {
   const [data, setData] = useState<PlatformSection[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   useEffect(() => {
     (async function fetchData() {
@@ -30,10 +33,43 @@ export default function HomePage() {
     })();
   }, []);
 
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+
+  const paginatedData = data.slice(startIndex, endIndex);
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) =>
+      Math.min(prevPage + 1, Math.ceil(data.length / PAGE_SIZE))
+    );
+  };
+
+
   return (
     <MainLayout>
+      <div className="mb-4">
+        <button
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1}
+          className="bg-blue-500 rounded-full px-4 py-1 text-white disabled:bg-neutral-400"
+        >
+          Previous
+        </button>
+        <span className="font-bold mx-4">Page {currentPage}</span>
+        <button
+          onClick={handleNextPage}
+          disabled={endIndex >= data.length}
+          className="bg-blue-500 rounded-full px-4 py-1 text-white disabled:bg-neutral-400"
+        >
+          Next
+        </button>
+      </div>
       <ul className="flex flex-col gap-y-8">
-        {data.map((el, idx) => {
+        {paginatedData.map((el, idx) => {
           return (
             <li
               key={`platform_section_${idx}`}
