a75027bb00d75ff64b06c791996d3d9b707e42ad
naarkz <naarkz@michie.dev>
2023-08-17 16:21:56 +0800

    refactor: Highlight active route; Componetize menu item

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+function MenuItem({ text, href }: { text: string; href: string }) {
+  const router = useRouter();
+  return (
+    <li
+      className={`${
+        router.pathname === href ? "text-blue-500" : "hover:text-neutral-600"
+      }`}
+    >
+      <Link href={href}>{text}</Link>
+    </li>
+  );
+}
 
 export default function Nav() {
   return (
     <nav className="w-full">
       <ul className="flex gap-x-5 p-6 font-bold">
-        <li className="hover:text-neutral-600">
-          <Link href={"/"}>Home</Link>
-        </li>
-        <li className="hover:text-neutral-600">
-          <Link href={"/about"}>About</Link>
-        </li>
+        <MenuItem href="/" text="Home" />
+        <MenuItem href="/about" text="About" />
       </ul>
     </nav>
   );
